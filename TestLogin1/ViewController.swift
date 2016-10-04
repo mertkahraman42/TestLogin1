fabf8651cd517e660a2feb335bcebb499fb39e02
Parasut <parasut@Parasuts-MacBook-Air.local>
2016-10-04 10:52:49 +0300

    Initial Commit

---
+++ TestLogin1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestLogin1
+//
+//  Created by Parasut on 04/10/2016.
+//  Copyright © 2016 Parasut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
